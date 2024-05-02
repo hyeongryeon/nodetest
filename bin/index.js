336b768c31194841cee3e47db5faf544400b993e
HyeongRyeon <gudfus2911@gmail.com>
2024-05-02 10:01:56 +0900

    비밀 정보 보호

--- bin/index.js
+++ bin/index.js
@@ -2,11 +2,25 @@ const express=require('express')
 const app=express()
 const port= 5000
 
+const { User } = require('./model/User');
+
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://gudfus29112:1234@cluster0.bghyghp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
 }).then(()=>console.log('MongoDB connected...'))
   .catch(err => console.log(err)) 
 
   app.get('/',(req,res) => res.send('hello world'))
+
+  app.post('/register',(req, res) =>{
+
+    // 회원가입에 필요한 정보를 client에서 가져오면
+    // 해당 정보를 DB에 넣어줄 사전 작업
+    
+
+
+  })
+
+
+
 
   app.listen(port,() => console.log(`Example app listening on port ${port}!`))
--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 const express=require('express')
 const app=express()
 const port= 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require('./model/User');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://gudfus29112:1234@cluster0.bghyghp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
+mongoose.connect(config.mongoURI,{
 }).then(()=>console.log('MongoDB connected...'))
   .catch(err => console.log(err)) 
 
-  app.get('/',(req,res) => res.send('hello world'))
+  app.get('/',(req,res) => res.send('hello world 하이하이'))
+
+  app.post('/register',(req, res) =>{
+
+    // 회원가입에 필요한 정보를 client에서 가져오면
+    // 해당 정보를 DB에 넣어줄 사전 작업
+
+      const user = new User(req.body)
+
+      //몽고DB 메서드
+      user.save().then(res =>{ 
+        console.log('정훈석등록');
+      })  
+      .catch(err =>{
+        console.error(err);
+      });
+
+    }) 
+
+
+  
+
+
+
 
   app.listen(port,() => console.log(`Example app listening on port ${port}!`))
